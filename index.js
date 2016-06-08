741f84d12670d9cc748a58d3bdab4ba241c59da3
James Lee <uptownhr@gmail.com>
2016-06-08 14:07:45 -0700

    added require cache purge for react components from the server

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ const React = require('react'),
   ReactDOMServer = require('react-dom/server'),
   {match, RouterContext} = require('react-router')
 
-const routes = require('./src/routes')
+
 
 app.get('/', (req,res) => {
-  console.log(req.url)
+  let id = require.resolve('./src/routes')
+  if(id) require.uncache('./src/routes')
+  console.log( require.cache[id] )
+
+  const routes = require('./src/routes')
+
+
   match({routes,location:req.url}, (err, redirect, renderProps) => {
-    console.log(err, renderProps, redirect)
     const element = React.createElement(RouterContext, renderProps)
     const template = `<html><body><div id='root'>${ReactDOMServer.renderToString(element)}</div><script src="http://localhost:3000/static/bundle.js"></script></body></html>`.replace(/\s+/g, ' ').trim();
     res.send(template)
@@ -22,3 +27,50 @@ app.get('/', (req,res) => {
 })
 
 app.listen(3000)
+
+
+
+/**
+ * Removes a module from the cache
+ */
+require.uncache = function (moduleName) {
+  // Run over the cache looking for the files
+  // loaded by the specified module name
+  require.searchCache(moduleName, function (mod) {
+    delete require.cache[mod.id];
+  });
+
+  // Remove cached paths to the module.
+  // Thanks to @bentael for pointing this out.
+  Object.keys(module.constructor._pathCache).forEach(function(cacheKey) {
+    if (cacheKey.indexOf(moduleName)>0) {
+      delete module.constructor._pathCache[cacheKey];
+    }
+  });
+};
+
+/**
+ * Runs over the cache to search for all the cached
+ * files
+ */
+require.searchCache = function (moduleName, callback) {
+  // Resolve the module identified by the specified name
+  var mod = require.resolve(moduleName);
+
+  // Check if the module has been resolved and found within
+  // the cache
+  if (mod && ((mod = require.cache[mod]) !== undefined)) {
+    // Recursively go over the results
+    (function run(mod) {
+      // Go over each of the module's children and
+      // run over it
+      mod.children.forEach(function (child) {
+        run(child);
+      });
+
+      // Call the specified callback providing the
+      // found module
+      callback(mod);
+    })(mod);
+  }
+};
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+
 export default class Home extends Component {
   constructor(){
     super()
@@ -13,14 +14,14 @@ export default class Home extends Component {
   componentDidMount(){
     console.log('component did mount')
     setTimeout(()=>{
-      this.setState({name: 'zzzzdfdfdfd'})
+      this.setState({name: 'zzzzdfdfdsdfsdfsdf'})
     }, 1000)
   }
   render() {
     console.log('render')
     // require the logo image both from client and server
     return (
-      <div> {this.state.name}</div>
+      <div> {this.state.name}z</div>
     );
   }
 }
