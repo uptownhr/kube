e2b6a7cc50a44c7bf032982bda6182ee2dcb7f9a
James Lee <uptownhr@gmail.com>
2016-06-09 11:58:18 -0700

    Added mongoose connection

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const {uncache}= require('./util')
 const express = require('express'),
   app = express(),
   { server, hot } = require('./server')
+const mongoose = require('mongoose')
 
+mongoose.connect("mongodb://localhost:27017/preax")
 
+mongoose.connection.on('error', () => {
+  console.log('Mongodb connection error')
+  process.exit(1)
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongodb connected')
+})
 
 app.use( server, hot )
 
