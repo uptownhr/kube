fd1db772c69666661be92137e582e9508a6ac2e4
James Lee <uptownhr@gmail.com>
2016-06-10 23:45:41 -0700

    removed mongoose

--- bootstrap.js
+++ bootstrap.js
@@ -3,17 +3,6 @@ const express = require('express'),
 
 require('./kube/kube-middleware')(app)
 
-const mongoose = require('mongoose')
-
-mongoose.connection.on('error', () => {
-  console.log('Mongodb connection error')
-  process.exit(1)
-})
-
-mongoose.connection.on('connected', () => {
-  console.log('Mongodb connected')
-})
-
 
 module.exports = {
   app, mongoose
