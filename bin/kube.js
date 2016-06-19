6424268a11b8fbe74c63e2bac4191782fd98b752
James Lee <uptownhr@gmail.com>
2016-06-18 18:31:39 -0700

    working on basic demo

--- bin/kube.js
+++ bin/kube.js
@@ -8,23 +8,16 @@ const path = require('path'),
 const kube_path = path.resolve(__dirname + '/../'),
   project_path = process.cwd()
 
-const kube_default = {
+//get kuberc
+const kuberc = require(project_path + '/.kuberc')
+const options = {
   kube_path,
   project_path,
-  project_entry: project_path + '/src/index.js',
-  project_entry_component: false,
-  project_routes: project_path + '/src/routes.js',
-  project_public_path: project_path + '/public'
+  client_entry: path.resolve( project_path + '/' + kuberc.client_entry ),
+  server_entry: path.resolve( project_path + '/' + kuberc.server_entry )
 }
 
-let options = {
-  project_routes: false,
-  project_entry_component: project_path + '/src/components/App.js'
-}
-options = {}
-options = Object.assign({},kube_default,options)
-
-
+console.log(options)
 
 const {dev, hot, ssr} = server(options)
 
