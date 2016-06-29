fa9da2ead6fbad99a4ef75e5c7625f4ffed9aeda
James Lee <uptownhr@gmail.com>
2016-06-28 18:10:04 -0700

    added fake favicon

--- bin/boilerplate/router-redux/layout.js
+++ bin/boilerplate/router-redux/layout.js
@@ -1,14 +1,17 @@
 module.exports = function ({renderString, bundlePath, stylePath, stateString }) {
   return `
 <html>
-
-<link rel="stylesheet" href="${stylePath}">
+<head>
+  <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
+  <link rel="stylesheet" href="${stylePath}">
+</head>
 <body>
-<div id='root'>${renderString}</div>
-<script>
-window.__INITIAL_STATE = ${stateString}
-</script>
-<script src="${bundlePath}"></script>
+  <div id='root'>${renderString}</div>
+  <script>
+    window.__INITIAL_STATE = ${stateString}
+  </script>
+  <script src="${bundlePath}"></script>
 </body>
 </html>`
 }
